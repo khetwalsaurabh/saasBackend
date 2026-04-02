1968b5c85079d98ccd8e2abd937133cad7df5d3f
khetwalsaurabh <ssourabh.1712@gmail.com>
2026-04-02 16:31:31 +0530

    fix uploads.js code static path

--- Multer/upload.js
+++ Multer/upload.js
@@ -2,15 +2,16 @@ const multer = require("multer");
 
 const storage = multer.diskStorage({
    destination: function (req, file, cb) {
-      cb(null, "uploads/logo"); // yha bhi path change cb(null, "/var/www/uploads/logo");
+      // cb(null, "uploads/logo"); // yha bhi path change cb(null, "/var/www/uploads/logo");
+      cb(null, "/var/www/uploads/logo");
    },
-   filename: function (req, file, cb) {
-      cb(null, Date.now() + "-" + file.originalname);
-   }
-
-   //   filename: function (req, file, cb) {
-   //   cb(null, Date.now() + "-" + file.originalname.replace(/\s+/g, "_"));
+   // filename: function (req, file, cb) {
+   //    cb(null, Date.now() + "-" + file.originalname);
    // }
+
+     filename: function (req, file, cb) {
+     cb(null, Date.now() + "-" + file.originalname.replace(/\s+/g, "_"));
+   }
 
 });
 
